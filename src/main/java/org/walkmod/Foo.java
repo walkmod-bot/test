3beb27dbf4ee28a1e89523e49a821ed1d3392462
rpau <raquelpau@gmail.com>
2014-12-14 20:38:15 +0100

    init

---
+++ src/main/java/org/walkmod/Foo.java
@@ -0,0 +1,5 @@
+package org.walkmod;
+
+public class Foo {
+
+}
---
+++ src/test/java/org/walkmod/FooTest.java
@@ -0,0 +1,15 @@
+package org.walkmod;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FooTest extends TestCase{
+	
+	@Test
+	public void test(){
+		Assert.assertTrue(true);
+	}
+
+}
