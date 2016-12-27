26a15b7132676560d209c77cb5602a0938d0b862
rpau <rpau@ac.upc.edu>
2016-12-27 17:41:15 +0100

    Bar

---
+++ src/main/java/org/walkmod/Bar.java
@@ -0,0 +1,36 @@
+package org.walkmod;
+
+public class Bar {
+
+   public void goToTeaserFrame() {
+   }
+
+   public boolean eval() {
+      return false;
+   }
+
+   public int other() {
+      return 0;
+   }
+
+   public void foo2() throws Exception {
+      boolean find = false;
+      int retry = 0;
+      while (!find) {
+         goToTeaserFrame();
+         find = eval();
+         if (!find && (++retry >= other())){
+           throw new Exception("Unable to access to the text teaser");
+         }
+      }
+      System.out.println("hello");
+      switch (retry) {
+      case 0:              
+            {
+                  System.out.println("hello");
+                  System.out.println("bye");
+            }        
+      }
+
+   }
+}
