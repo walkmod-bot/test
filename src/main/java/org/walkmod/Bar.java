9452ca4219e89a1a65724d327fb93d8263544e32
rpau <rpau@ac.upc.edu>
2016-12-28 12:40:36 +0100

    bar

--- src/main/java/org/walkmod/Bar.java
+++ src/main/java/org/walkmod/Bar.java
@@ -24,7 +24,7 @@ public class Bar {
          }
       }
       System.out.println("hello");
-      System.out.println("bar8");
+      System.out.println("bar10");
       switch (retry) {
       case 0:              
             {
